593e44d348338bd6b1100be0abaf346ddbf16656
ADITYA RAJ <164489231+adityaraj63@users.noreply.github.com>
2026-06-24 14:36:37 +0530

    Time: 0 ms (100%), Space: 46.4 MB (54.07%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,13 +1,25 @@
+// Algorithm :
 
+// 1. Create odd pointer at head.
+// 2. Create even pointer at head.next.
+// 3. Save evenHead.
+// 4. Connect odd nodes together.
+// 5. Connect even nodes together.
+// 6. Move odd and even pointers.
+// 7. After loop, connect odd list with evenHead.
+// 8. Return head.
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) return head;
+
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead = even;
+
         while (even != null && even.next != null) {
             odd.next = even.next;
             odd = odd.next;
+
             even.next = odd.next;
             even = even.next;
         }
