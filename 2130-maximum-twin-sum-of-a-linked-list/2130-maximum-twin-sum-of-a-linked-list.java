9cf874ae63d56819c1ceb9868ea06c51804f3747
ADITYA RAJ <164489231+adityaraj63@users.noreply.github.com>
2026-06-24 12:49:40 +0530

    Time: 4 ms (96.93%), Space: 101.1 MB (78.48%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -1,33 +1,36 @@
 class Solution {
     public int pairSum(ListNode head) {
-        
         ListNode slow = head;
         ListNode fast = head;
-        
+
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
+// REVERSE THE HALF PART OF GIVEN LL AFTER FINDING MIDLLE ELEMENT SO WE CAN BE ABLE TO FIND THE MAXIMUM TWIN PAIR , SO ONCE WE REVERSE HALF PART OF GIVEN LL , THEN WE CAN ADD ORIGINAL LL WITH HALF REVERSED LL:
 
-        
-        ListNode prev = null;
-        while (slow != null) {
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
+            ListNode prev = null;
+            ListNode curr = slow;
+
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+
+// NOW CLACULATE MAXIMUM TWIN SUM:
+                ListNode first = head;
+                ListNode second = prev;
+                int maxPairSum = 0;
+
+                while (second != null) {
+                    maxPairSum = Math.max(maxPairSum , first.val + second.val);
+
+                    first = first.next;
+                    second = second.next;      
+                }
+                return maxPairSum;
+            }
         }
-
-        int maxSum = 0;
-        ListNode first = head;
-        ListNode second = prev;
-
-        while (second != null) {
-            maxSum = Math.max(maxSum, first.val + second.val);
-            first = first.next;
-            second = second.next;
-        }
-
-        return maxSum;
-    }
-}
